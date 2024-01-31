9aca3d3e0348290a1363fd6224c97008bd7cdbd4
John Monsen <598Johnn897@gmail.com>
2024-01-31 15:20:14 -0500

    Update calibrate.py

--- calibrate.py
+++ calibrate.py
@@ -12,4 +12,4 @@ odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 while odrv0.axis0.current_state != AXIS_STATE_IDLE:
     time.sleep(0.1)
 
-odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv0.axis0.requested_state = AXIS_STATE_IDLE
