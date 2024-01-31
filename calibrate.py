49b05a12071f8eac250fc05db7baba23208e2b3c
John Monsen <598Johnn897@gmail.com>
2024-01-31 15:19:10 -0500

    Update calibrate.py

--- calibrate.py
+++ calibrate.py
@@ -1,3 +1,9 @@
+import odrive
+from odrive.enums import *
+
+import time
+from time import sleep, perf_counter_ns
+
 print("starting calibration...")
 odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 while odrv0.axis0.current_state != AXIS_STATE_IDLE:
